1b5d82899e8df9502d0e65cd29f2035e9d285c4d
김종권 <jongkwon.kim@42dot.com>
2021-10-19 22:26:50 +0900

    Fix API latency

--- PopUpEx/API.swift
+++ PopUpEx/API.swift
@@ -19,7 +19,7 @@ struct ErrorData {
 
 struct API {
     static func someAPI(completion: @escaping (Result<Void, ErrorType>) -> ()) {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
             completion(.failure(ErrorType.notConnectedToInternet(ErrorData(title: "네트워크 연결 에러입니다.\n네트워크 설정을 확인해주세요", message: ""))))
         }
     }
